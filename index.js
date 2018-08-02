d8294a118bf1f58b1d657bca1ecfaac778644ca3
Gabriel Cardona <gabriel@bitcoin.com>
2018-08-02 20:55:52 +0800

    Clean up, set fees and POST tx.

--- index.js
+++ index.js
@@ -6,20 +6,13 @@ module.exports = class WormholeCash {
     this.mnemonic = mnemonic;
     this.hdpath = hdpath;
     let rootSeed = BITBOX.Mnemonic.toSeed(mnemonic);
-
-    // master HDNode
     let masterHDNode = BITBOX.HDNode.fromSeed(rootSeed, 'bitcoincash');
-
-    // HDNode of BIP44 account
-    let account = BITBOX.HDNode.derivePath(masterHDNode, "m/44'/145'/0'");
-
-    // derive the first external change address HDNode which is going to spend utxo
-    this.change = BITBOX.HDNode.derivePath(account, "0/0");
+    this.change = BITBOX.HDNode.derivePath(masterHDNode, "m/44'/145'/0'/0/0");
     this.cashAddress = BITBOX.HDNode.toCashAddress(this.change);
   }
 
   burn(satoshis) {
-    BITBOX.Address.utxo('bitcoincash:qzs02v05l7qs5s24srqju498qu55dwuj0cx5ehjm2c').then((result) => {
+    BITBOX.Address.utxo(this.cashAddress).then((result) => {
       if(!result[0]) {
         return;
       }
@@ -38,14 +31,11 @@ module.exports = class WormholeCash {
       // add input with txid and index of vout
       transactionBuilder.addInput(txid, vout);
 
-      // get byte count to calculate fee. paying 1 sat/byte
-      let byteCount = BITBOX.BitcoinCash.getByteCount({ P2PKH: 1 }, { P2PKH: 1 });
-      // 192
       // amount to send to receiver. It's the original amount - 1 sat/byte for tx size
-      let sendAmount = originalAmount - byteCount;
+      let sendAmount = originalAmount - 211;
 
       // add output w/ address and amount to send
-      transactionBuilder.addOutput('qqqqqqqqqqqqqqqqqqqqqqqqqqqqqu08dsyxz98whc', satoshis);
+      transactionBuilder.addOutput('qqqqqqqqqqqqqqqqqqqqqqqqqqqqqu08dsyxz98whc', sendAmount);
       // let data = Buffer.from("0877686300000044");
       let data = "0877686300000044";
       let buf = BITBOX.Script.nullData.output.encode(Buffer.from(data, 'hex'));
@@ -65,13 +55,11 @@ module.exports = class WormholeCash {
       console.log(hex);
 
       // sendRawTransaction to running BCH node
-      // BITBOX.RawTransactions.sendRawTransaction(hex).then((result) => {
-      //   this.setState({
-      //     txid: result
-      //   });
-      // }, (err) => {
-      //   console.log(err);
-      // });
+      BITBOX.RawTransactions.sendRawTransaction(hex).then((result) => {
+        console.log(result);
+      }, (err) => {
+        console.log(err);
+      });
     }, (err) => {
       console.log(err);
     });
