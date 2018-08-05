18fb9453c713790075aee2b41a9e19ffee59a5a2
Gabriel Cardona <gabriel@bitcoin.com>
2018-08-05 16:32:03 -0700

    Add Transaction.

--- src/Wormhole.js
+++ src/Wormhole.js
@@ -3,6 +3,7 @@ import Configuration from './Configuration';
 import DataRetrieval from './DataRetrieval';
 import PayloadCreation from './PayloadCreation';
 import RawTransactions from './RawTransactions';
+import Transaction from './Transaction';
 
 class Wormhole {
   constructor(config) {
@@ -16,6 +17,7 @@ class Wormhole {
     this.DataRetrieval = new DataRetrieval(this.restURL);
     this.PayloadCreation = new PayloadCreation(this.restURL);
     this.RawTransactions = new RawTransactions(this.restURL);
+    this.Transaction = new Transaction(this.restURL);
 
     this.BITBOX = new BITBOXCli();
   }
